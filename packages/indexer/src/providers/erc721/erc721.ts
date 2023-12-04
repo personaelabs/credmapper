7ab9e9b579f21a2ca45e623c2f7ccbb216341c53
Daniel Tehrani <contact@dantehrani.com>
2023-12-04 11:10:34 -0800

    chore: small erc721 indexing improvement

--- packages/indexer/src/providers/erc721/erc721.ts
+++ packages/indexer/src/providers/erc721/erc721.ts
@@ -2,14 +2,11 @@ import { TransferEvent } from '@prisma/client';
 import prisma from '../../prisma';
 import { GetFilterLogsReturnType, Hex, Chain, PublicClient, HttpTransport } from 'viem';
 import { processLogs } from '../../lib/processLogs';
-import * as chains from 'viem/chains';
 import CONTRACT_EVENTS from './contracts';
 import { TRANSFER_EVENT } from './abi/abi';
 import { ContractWithDeployedBlock } from '../../types';
-import { NUM_MAINNET_CLIENTS, getClient } from '../ethRpc';
 import { runInParallel } from '../../utils';
 
-// Sync `Transfer` events from ERC721 contracts
 const indexTransferEvents = async (
   client: PublicClient<HttpTransport, Chain>,
   contract: ContractWithDeployedBlock,
@@ -26,40 +23,34 @@ const indexTransferEvents = async (
   const fromBlock = latestSyncedEvent?._max.blockNumber || BigInt(contract.deployedBlock);
 
   const processTransfers = async (logs: GetFilterLogsReturnType) => {
-    const data = (
-      await Promise.all(
-        logs.map((log) => {
-          // @ts-ignore
-          const from = log.args.from;
-          // @ts-ignore
-          const to = log.args.to;
-          // @ts-ignore
-          const tokenId = log.args.tokenId;
+    const data = logs.map((log) => {
+      // @ts-ignore
+      const from = log.args.from;
+      // @ts-ignore
+      const to = log.args.to;
+      // @ts-ignore
+      const tokenId = log.args.tokenId;
 
-          const transactionIndex = log.transactionIndex;
-          const logIndex = log.logIndex;
+      const transactionIndex = log.transactionIndex;
+      const logIndex = log.logIndex;
 
-          if (from && to && tokenId != null) {
-            return {
-              contractId: contract.id,
-              from: from.toLowerCase() as Hex,
-              to: to.toLowerCase() as Hex,
-              tokenId: tokenId,
-              blockNumber: log.blockNumber,
-              transactionIndex: transactionIndex,
-              logIndex: logIndex,
-            };
-          } else {
-            return false;
-          }
-        }),
-      )
-    ).filter((data) => data) as TransferEvent[];
-
-    await prisma.transferEvent.createMany({
-      data,
-      skipDuplicates: true,
+      return {
+        contractId: contract.id,
+        from: from.toLowerCase() as Hex,
+        to: to.toLowerCase() as Hex,
+        tokenId: tokenId,
+        blockNumber: log.blockNumber,
+        transactionIndex: transactionIndex,
+        logIndex: logIndex,
+      } as TransferEvent;
     });
+
+    if (data.length > 0) {
+      await prisma.transferEvent.createMany({
+        data,
+        skipDuplicates: true,
+      });
+    }
   };
 
   await processLogs(client, TRANSFER_EVENT, fromBlock, processTransfers, contract, BigInt(1000));
